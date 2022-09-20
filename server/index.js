7fc12b7743db8fd6b5952074fac1095597e667f3
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-20 19:41:53 +0000

    working on POST requests to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const db = require('./db');
 
 const app = express();
 
@@ -9,6 +10,32 @@ app.use(staticMiddleware);
 
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.post('/api/artists', (req, res) => {
+  const name = req.body.name;
+  if (!name) {
+    res.status(400).json({
+      error: 'name is a required field'
+    });
+    return;
+  }
+  const sql = `
+  insert into "artists" ("name")
+  values      ('$1')`;
+
+  const params = [name];
+  db.query(sql, params)
+    .then(result => {
+      const [artistName] = result.rows;
+      res.status(201).json(artistName);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'an unexpected error occured'
+      });
+    });
 });
 
 app.use(errorMiddleware);
