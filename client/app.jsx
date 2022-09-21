0edff25e16a15cc22c00e7121a520497d1211615
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-21 19:30:08 +0000

    working o form functionality)

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import Home from './pages/home';
+// import React from 'react';
+// import Home from './pages/home';
+// import Form from './pages/form';
 
-export default class App extends React.Component {
-  render() {
-    return <Home />;
-  }
-}
+// export default class App extends React.Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       artists: []
+//     }
+//     this.addName = this.addName.bind(this);
+//   }
+
+//   addName(newArtist){
+//     fetch('/api/artists',{
+//       method: 'POST',
+//       headers:{
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(newArtist)
+//     })
+//     .then(response => response.json())
+//     .then(name =>{
+//       this.setState({
+//         name: this.state.artists.concat(artists)
+//       })
+//     })
+//     .catch(console.error)
+//   }
+//   render() {
+//     return(
+//       <div className="container">
+//         <Form onSubmit={this.addName} />
+//       </div>
+//     )
+//   }
+// }
