b53459455e4b442d46b9cc4b5ecc17a377ba6e8c
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-22 01:03:43 +0000

    still working on first page appearance

--- client/pages/form.jsx
+++ client/pages/form.jsx
@@ -1,50 +1,51 @@
-// import React from 'react';
-// //import ReactDom from 'react-dom'
+import React from 'react';
+// import ReactDom from 'react-dom'
 
-// export default class Form extends React.Component {
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       name: '',
-//       route: parseRoute(window.location.hash)
-//     }
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+      // route: parseRoute(window.location.hash)
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-//   handleChange(event){
-//     this.setState({
-//       name: event.target.value
-//     })
-//   }
+  handleChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
 
-//   handleSubmit(event){
-//     event.preventDefault();
-//     const newArtist ={
-//       name: this.state.name
-//     }
-//     this.props.onSubmit(newArtist);
-//     this.setState({
-//       name: ''
-//     })
-//   }
-//   render() {
-//     return (
-//       <div className="d-flex justify-content-center form-wrapper">
-//         <form
-//           className="d-flex justify-content-center align-self-center align-items-center flex-column"
-//           onSubmit={ this.handleSubmit} >
-//           <div className="form-group">
-//             <label htmlFor="new-artist">Add a new artist.</label>
-//           </div>
-//           <div className="form-group">
-//             <input name="new-artist" type="text" className="form-control col-12" id="new-artist" required/>
-//           </div>
-//           <div className="">
-//             <button type="submit" className="btn new-artist-submit">Submit</button>
-//           </div>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
+  handleSubmit(event) {
+    event.preventDefault();
+    const newArtist = {
+      name: this.state.name
+    };
+    this.props.onSubmit(newArtist);
+    this.setState({
+      name: ''
+    });
+  }
+
+  render() {
+    return (
+      <div className="d-flex justify-content-center form-wrapper">
+        <form
+          className="d-flex justify-content-center align-self-center align-items-center flex-column"
+          onSubmit={ this.handleSubmit} >
+          <div className="form-group">
+            <label htmlFor="new-artist">Add a new artist.</label>
+          </div>
+          <div className="form-group">
+            <input name="new-artist" type="text" className="form-control col-12" id="new-artist" required/>
+          </div>
+          <div className="">
+            <button type="submit" className="btn new-artist-submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
