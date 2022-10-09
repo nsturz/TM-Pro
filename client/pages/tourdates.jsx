28378ae6a3eae7d1f657b87f93d849d17fb85244
Nicolas Sturz <nicksturz92@gmail.com>
2022-10-09 22:34:34 +0000

    saving

--- client/pages/tourdates.jsx
+++ client/pages/tourdates.jsx
@@ -5,20 +5,25 @@ export default class TourDates extends React.Component {
     return (
       <div className="container calendar-container">
         <div className="d-flex justify-content-between row p-2">
-          <h3>North American Tour 2023</h3>
-          <a href="#"><i className="fa-solid fa-plus pt-1" /></a>
+          <h3 className="m-3">North American Tour 2023</h3>
+          <a href="#"><i className="fa-solid fa-plus pt-1 m-3" /></a>
         </div>
         <div className="row">
-          <ul>
-            <li>
+          <ul className="col-12">
+            <li className="calendar-list-item">
               <div className="row">
-                <div className="col-2 calendar-date">
-                  <p className="text-center">MON</p>
-                  <p className="text-center"> MAY 1 </p>
+                <div className="col-1 ml-3 calendar-date">
+                  <p className="calendar-date-text text-center font-weight-bold d-block text-white m-0">
+                    MON MAY 1
+                  </p>
                 </div>
-                <div className="col-8">
-                  <p className="text-white">Anaheim, CA</p>
-                  <p className="text-white">Chain Reaction</p>
+                <div className="col-9">
+                  <p className="text-white font-weight-bold m-0">Anaheim, CA</p>
+                  <pre className="text-white font-weight-light font-italic m-0">Chain Reaction</pre>
+                </div>
+                <div className="col-1 ml-5">
+                  <a href=""><i className="fa-solid fa-trash m-2 " /></a>
+                  <a href=""><i className="fa-solid fa-pen-to-square m-2" /></a>
                 </div>
               </div>
             </li>
