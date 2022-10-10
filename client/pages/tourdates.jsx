e71b7dff25b5ba5c89df760abd967cf3ee7e5a5b
Nicolas Sturz <nicksturz92@gmail.com>
2022-10-10 21:48:35 +0000

    finished styling for TourDates component; both desktop and mobile

--- client/pages/tourdates.jsx
+++ client/pages/tourdates.jsx
@@ -4,27 +4,25 @@ export default class TourDates extends React.Component {
   render() {
     return (
       <div className="container calendar-container">
-        <div className="d-flex justify-content-between row p-2">
-          <h3 className="m-3">North American Tour 2023</h3>
+        <div className="d-flex justify-content-lg-between row p-2">
+          <h3 className="m-3 col-9">North American Tour 2023</h3>
           <a href="#"><i className="fa-solid fa-plus pt-1 m-3" /></a>
         </div>
         <div className="row">
           <ul className="col-12">
-            <li className="calendar-list-item">
-              <div className="row">
-                <div className="col-1 ml-3 calendar-date">
-                  <p className="calendar-date-text text-center font-weight-bold d-block text-white m-0">
-                    MON MAY 1
-                  </p>
-                </div>
-                <div className="col-9">
-                  <p className="text-white font-weight-bold m-0">Anaheim, CA</p>
-                  <pre className="text-white font-weight-light font-italic m-0">Chain Reaction</pre>
-                </div>
-                <div className="col-1 ml-5">
-                  <a href=""><i className="fa-solid fa-trash m-2 " /></a>
-                  <a href=""><i className="fa-solid fa-pen-to-square m-2" /></a>
-                </div>
+            <li className="row calendar-list-item">
+              <div className="col-3 col-lg-1 ml-3 calendar-date">
+                <p className="calendar-date-text text-center font-weight-bold d-block text-white m-0">
+                  MON MAY 1
+                </p>
+              </div>
+              <div className="col-5 col-lg-8 ">
+                <p className="text-white font-weight-bold m-0">Anaheim, CA</p>
+                <pre className="text-white font-weight-light font-italic m-0">Chain Reaction</pre>
+              </div>
+              <div className="col-2 col-lg-2 ml-5">
+                <a href=""><i className="fa-solid fa-trash m-2 " /></a>
+                <a href=""><i className="fa-solid fa-pen-to-square m-2" /></a>
               </div>
             </li>
           </ul>
