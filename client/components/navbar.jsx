6c87369c3541fa23f69825ccd0c63ea6f6e28595
Nicolas Sturz <nicksturz92@gmail.com>
2023-04-23 21:56:19 +0000

    began styling for new dashboard page

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -4,7 +4,7 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg">
           <a className="navbar-brand ml-5" href="#">TOUR MANAGER PRO</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -40,80 +40,97 @@ export default class Dashboard extends React.Component {
     // console.log('this.state.testSchedules:', this.state.testSchedules)
     const { city, date, line1, state, venueName, notesDetails, contactEmail, contactPhone, contactName } = this.state.show;
     return (
-      <div className="dashboard-container d-flex justify-center flex-wrap mt-4 mr-3 ml-3">
-        <h3>BIG UPDATE COMING!!!</h3>
-        <h3 className="col-12 mb-5">{ date } - { city }, { state }</h3>
-        <div className="venues-notes-wrapper col-lg-3 col-12 mh-100">
-          <div className="mb-3 mt-3">
-            <h6 className="d-inline">VENUE</h6>
-            <i className="fa-solid fa-location-dot mt-1 info" />
-          </div>
-          <div className="venues col-12 info p-3" id="venue">
-            <div className="row d-block">
-              <p className="text-center lead">{venueName}</p>
-              <p className="text-center lead">{line1}</p>
-              <p className="text-center lead">{city}, {state}</p>
-            </div>
-          </div>
-          <div className="mb-3 mt-3">
-            <h6 className="d-inline">NOTES</h6>
-            <i className="fa-solid fa-clipboard-list mt-1 info" />
-          </div>
-          <div className="notes col-12 info" id="notes">
-            <div className="row mt-3">
-              <pre className="pl-3">{notesDetails}</pre>
+      <div className="DELETE">
+        <div className="container d-flex justify-center">
+          <div className="col-12 m-1">
+            <div className="row">
+              <div className="col">
+                <hr className="hr-new" />
+              </div>
+              <div className="col m-0 p-0 text-center">
+                <i className="fa-solid fa-calendar-days d-inline m-1 info-new" />
+                <h6 className="date-new m-0 d-inline m-1 ">Feb 8th, 2023</h6>
+              </div>
+              <div className="col">
+                <hr className="hr-new" />
+              </div>
             </div>
           </div>
         </div>
-        <div className="schedule-wrapper col-lg-3 col-12 mh-100 " id="schedule">
-          <div className="mb-3 mt-3">
-            <h6 className="d-inline">SCHEDULE</h6>
-            <i className="fa-solid fa-clock info" />
+        <div className="dashboard-container d-flex justify-center flex-wrap mt-4 mr-3 ml-3">
+          <h3 className="col-12 mb-5">{date} - {city}, {state}</h3>
+          <div className="venues-notes-wrapper col-lg-3 col-12 mh-100">
+            <div className="mb-3 mt-3">
+              <h6 className="d-inline">VENUE</h6>
+              <i className="fa-solid fa-location-dot mt-1 info" />
+            </div>
+            <div className="venues col-12 info p-3" id="venue">
+              <div className="row d-block">
+                <p className="text-center lead">{venueName}</p>
+                <p className="text-center lead">{line1}</p>
+                <p className="text-center lead">{city}, {state}</p>
+              </div>
+            </div>
+            <div className="mb-3 mt-3">
+              <h6 className="d-inline">NOTES</h6>
+              <i className="fa-solid fa-clipboard-list mt-1 info" />
+            </div>
+            <div className="notes col-12 info" id="notes">
+              <div className="row mt-3">
+                <pre className="pl-3">{notesDetails}</pre>
+              </div>
+            </div>
           </div>
-          <div className=" schedules col-12 h-100">
-            <ul className="pl-2">
-              {
-                this.state.schedules.map(event => {
-                  return (
-                    <li className="text-white" key={event.scheduleId}>
-                      {event.startTime} - {event.endTime} {event.scheduleDetails}
-                    </li>
-                  );
-                })
-              }
-            </ul>
+          <div className="schedule-wrapper col-lg-3 col-12 mh-100 " id="schedule">
+            <div className="mb-3 mt-3">
+              <h6 className="d-inline">SCHEDULE</h6>
+              <i className="fa-solid fa-clock info" />
+            </div>
+            <div className=" schedules col-12 h-100">
+              <ul className="pl-2">
+                {
+                  this.state.schedules.map(event => {
+                    return (
+                      <li className="text-white" key={event.scheduleId}>
+                        {event.startTime} - {event.endTime} {event.scheduleDetails}
+                      </li>
+                    );
+                  })
+                }
+              </ul>
 
+            </div>
           </div>
-        </div>
-        <div className="contacts-wrapper col-lg-3 col-12" id="contacts">
-          <div className="mb-3 mt-3">
-            <h6 className="d-inline">CONTACTS</h6>
-            <i className="fa-solid fa-phone info" />
+          <div className="contacts-wrapper col-lg-3 col-12" id="contacts">
+            <div className="mb-3 mt-3">
+              <h6 className="d-inline">CONTACTS</h6>
+              <i className="fa-solid fa-phone info" />
+            </div>
+            <div className="contacts col-12 h-50 info p-3">
+              <p className="pl-2">{contactEmail} </p>
+              <p className="pl-2">{contactName}</p>
+              <p className="pl-2">{contactPhone}</p>
+            </div>
           </div>
-          <div className="contacts col-12 h-50 info p-3">
-            <p className="pl-2">{ contactEmail } </p>
-            <p className="pl-2">{ contactName }</p>
-            <p className="pl-2">{ contactPhone }</p>
-          </div>
-        </div>
-        <div className="col-lg-3 col-12" id="dates">
-          <div className="mb-3 mt-3">
-            <h6 className='d-inline'>DATES</h6>
-            <i className="fa-solid fa-calendar-days info" />
-          </div>
-          <div className=" dates col-12 info p-0">
-            <ul>
-              {
-                this.props.tourDates.map(event => {
-                  return (
-                    <li className="row date-wrapper pl-3 " key={event.showId}>
-                      <a href="#" className="date col-6 ">{event.showDate}</a>
-                      <a href="#" className="city"><b>{event.dateCity},{event.dateState} </b><br />{event.dateVenue}</a>
-                    </li>
-                  );
-                })
-              }
-            </ul>
+          <div className="col-lg-3 col-12" id="dates">
+            <div className="mb-3 mt-3">
+              <h6 className='d-inline'>DATES</h6>
+              <i className="fa-solid fa-calendar-days info" />
+            </div>
+            <div className=" dates col-12 info p-0">
+              <ul>
+                {
+                  this.props.tourDates.map(event => {
+                    return (
+                      <li className="row date-wrapper pl-3 " key={event.showId}>
+                        <a href="#" className="date col-6 ">{event.showDate}</a>
+                        <a href="#" className="city"><b>{event.dateCity},{event.dateState} </b><br />{event.dateVenue}</a>
+                      </li>
+                    );
+                  })
+                }
+              </ul>
+            </div>
           </div>
         </div>
       </div>
