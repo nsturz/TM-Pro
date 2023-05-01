9e82926af0aae211e005792a52a6cfc0a7d5bf36
Nicolas Sturz <nicksturz92@gmail.com>
2023-05-01 17:27:35 +0000

    working on schedule rendering, need to research more

--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -8,127 +8,106 @@ export default class Dashboard extends React.Component {
     super(props);
     this.state = {
       tourDates: [],
-      show: '',
       notes: '',
       contacts: '',
-      schedules: [],
-      testSchedules: [],
-      reference: []
+      schedules: []
     };
 
   }
 
   componentDidMount() {
-    fetch('api/shows')
+    fetch('api/all-schedules')
       .then(res => res.json())
-      .then(tourDates => this.setState({ tourDates }));
+      .then(schedules => this.setState({ schedules }));
 
     fetch('/api/all-shows')
       .then(res => res.json())
       .then(tourDates => this.setState({ tourDates }));
 
-    fetch('/api/shows/1')
-      .then(res => res.json())
-      .then(show => this.setState({ show }));
-
-    fetch('api/schedules')
-      .then(res => res.json())
-      .then(schedules => this.setState({ schedules }));
-
-    fetch('api/all-schedules')
-      .then(res => res.json())
-      .then(testSchedules => this.setState({ testSchedules }));
   }
 
   render() {
     // console.log('this.state.tourDates:', this.state.tourDates)
-    // console.log('this.state.tourDates:', this.state.tourDates)
     // console.log('this.state.schedules:', this.state.schedules)
-    // console.log('this.state.testSchedules:', this.state.testSchedules)
-    // console.log('this.state.reference:', this.state.reference)
     // const tourDates = this.state.tourDates;
-    const { city, date, line1, state, venueName, notesDetails, contactEmail, contactPhone, contactName } = this.state.show;
+    // const schedules = this.state.schedules;
     return (
       <div className="DELETE" >
         <ul className="mt-3">
           {
-                this.state.tourDates.map(event => {
-                  this.state.reference.push(event.showId);
-                  return (
-                    <li className="tourDate container" key={event.showId} id={event.showId}>
-                      <div className="row mt-5">
-                        <div className="col-lg-5 p-0">
-                          <hr className="hr-new" />
+              this.state.tourDates.map(event => {
+                return (
+                  <li className="tourDate container" key={event.showId} id={event.showId}>
+                    <div className="row mt-5">
+                      <div className="col-lg-5 p-0">
+                        <hr className="hr-new" />
+                      </div>
+                      <div className="col-lg-2 m-0 p-0 text-center">
+                        <h6 className="poppins-dark m-0 d-inline m-1 ">{event.date}</h6>
+                        <i className="fa-solid fa-calendar-days d-inline m-1 info-new" />
+                      </div>
+                      <div className="col-lg-5 p-0">
+                        <hr className="hr-new" />
+                      </div>
+                    </div>
+                    <div className="details-container d-flex flex-wrap justify-content-center mt-3 mb-5">
+                      <div className="col-12 col-lg venues-new ml-2 mr-2">
+                        <div className="row">
+                          <h6 className="poppins-dark d-inline">Venue</h6>
+                          <i className="fa-solid fa-location-dot d-inline m-1 info-new" />
                         </div>
-                        <div className="col-lg-2 m-0 p-0 text-center">
-                          <h6 className="poppins-dark m-0 d-inline m-1 ">{event.date}</h6>
-                          <i className="fa-solid fa-calendar-days d-inline m-1 info-new" />
-                        </div>
-                        <div className="col-lg-5 p-0">
-                          <hr className="hr-new" />
+                        <div className="row">
+                          <div className="col pl-2 pr-2 pb-3 pt-3 box-shadow rounded overflow-y venue-details-new">
+                            <div>
+                              <p className="lato-dark m-0 ">{event.venueName}</p>
+                              <p className="lato-dark m-0"> {event.line1}</p>
+                              <p className="lato-dark m-0">{event.city}, {event.state}</p>
+                              <p className="lato-dark m-0">ZIP CODE</p>
+                            </div>
+                            <div className="d-flex justify-content-center mt-3">
+                              <img src="../images/google-map-image.png" alt="" />
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div className="details-container d-flex flex-wrap justify-content-center mt-3 mb-5">
-                        <div className="col-12 col-lg venues-new ml-2 mr-2">
-                          <div className="row">
-                            <h6 className="poppins-dark d-inline">Venue</h6>
-                            <i className="fa-solid fa-location-dot d-inline m-1 info-new" />
-                          </div>
-                          <div className="row">
-                            <div className="col pl-2 pr-2 pb-3 pt-3 box-shadow rounded overflow-y venue-details-new">
-                              <div>
-                                <p className="lato-dark m-0 ">{event.venueName}</p>
-                                <p className="lato-dark m-0"> {event.line1}</p>
-                                <p className="lato-dark m-0">{event.city}, {event.state}</p>
-                                <p className="lato-dark m-0">ZIP CODE</p>
+                      <div className="col-12 col-lg details-new ml-2 mr-2">
+                        <div className="row">
+                          <h6 className="poppins-dark d-inline ">Details</h6>
+                          <i className="fa-solid fa-clipboard-list d-inline m-1 info-new" />
+                        </div>
+                        <div className="row">
+                          <div className="col box-shadow rounded pt-3 pl-2 pr-2 notes-contacts-new">
+                            <div className="row">
+                              <div className="col">
+                                <p className="lato-dark">{event.contactName}</p>
+                                <p className="lato-dark">{event.contactPhone}</p>
+                                <p className="lato-dark">t{event.contactEmail}</p>
                               </div>
-                              <div className="d-flex justify-content-center mt-3">
-                                <img src="../images/google-map-image.png" alt="" />
+                            </div>
+                            <div className="row">
+                              <div className="col">
+                                <p className="lato-dark">{event.notesDetails}</p>
                               </div>
                             </div>
                           </div>
                         </div>
-                        <div className="col-12 col-lg details-new ml-2 mr-2">
-                          <div className="row">
-                            <h6 className="poppins-dark d-inline ">Details</h6>
-                            <i className="fa-solid fa-clipboard-list d-inline m-1 info-new" />
-                          </div>
-                          <div className="row">
-                            <div className="col box-shadow rounded pt-3 pl-2 pr-2 notes-contacts-new">
-                              <div className="row">
-                                <div className="col">
-                                  <p className="lato-dark">{event.contactName}</p>
-                                  <p className="lato-dark">{event.contactPhone}</p>
-                                  <p className="lato-dark">t{event.contactEmail}</p>
-                                </div>
-                              </div>
-                              <div className="row">
-                                <div className="col">
-                                  <p className="lato-dark">{event.notesDetails}</p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
+                      </div>
+                      <div className="col-12 col-lg schedule-new ml-2 mr-2 ">
+                        <div className="row">
+                          <h6 className="poppins-dark d-inline ">Schedule</h6>
+                          <i className="fa-solid fa-clock d-inline m-1 info-new" />
                         </div>
-                        <div className="col-12 col-lg schedule-new ml-2 mr-2 ">
-                          <div className="row">
-                            <h6 className="poppins-dark d-inline ">Schedule</h6>
-                            <i className="fa-solid fa-clock d-inline m-1 info-new" />
-                          </div>
-                          <div className="row">
-                            <div className="col box-shadow rounded schedule-details-new">
-                              <ul>
-                                {/* {
-                                  this.state.testSchedules.forEach(function(element, index){
-                                    if(element.showId === tourDates[index].showId){
-                                      return(
-                                        <li>something</li>
-                                      )
-                                    }
-                                  })
-                                } */}
-                              </ul>
-                              {/* <ul>
+                        <div className="row">
+                          <div className="col box-shadow rounded schedule-details-new">
+                            <ul>
+                              {
+                                // console.log('tourDates:', tourDates, 'testSchedules:', testSchedules)
+                                // tourDates.map((event, index) => {
+                                //   console.log('tourDates[index].showId', tourDates[index].showId, 'schedules[index].showId', schedules[index].showId)
+                                // })
+                              }
+                            </ul>
+                            {/* <ul>
                                 <li className="lato-dark">9:30am - 10:30am: Travel</li>
                                 <li className="lato-dark">4:00pm - 5:00pm Load In </li>
                                 <li className="lato-dark">6:30pm - Doors</li>
@@ -138,16 +117,16 @@ export default class Dashboard extends React.Component {
                                 <li className="lato-dark">4:00pm - 5:00pm Load In </li>
                                 <li className="lato-dark">6:30pm - Doors</li>
                               </ul> */}
-                            </div>
                           </div>
                         </div>
                       </div>
-                    </li>
-                  );
-                })
-              }
+                    </div>
+                  </li>
+                );
+              })
+            }
         </ul>
-        <div className="dashboard-container d-flex justify-center flex-wrap mt-4 mr-3 ml-3">
+        {/* <div className="dashboard-container d-flex justify-center flex-wrap mt-4 mr-3 ml-3">
           <h3 className="col-12 mb-5">{date} - {city}, {state}</h3>
           <div className="venues-notes-wrapper col-lg-3 col-12 mh-100">
             <div className="mb-3 mt-3">
@@ -222,7 +201,7 @@ export default class Dashboard extends React.Component {
               </ul>
             </div>
           </div>
-        </div>
+        </div> */}
       </div >
     );
   }
