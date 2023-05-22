57844f2ea4dc9d33d6b44be88c0aa794b3c9394a
Nicolas Sturz <nicksturz92@gmail.com>
2023-05-22 17:54:46 +0000

    saving

--- client/components/new-tour-date.jsx
+++ client/components/new-tour-date.jsx
@@ -221,7 +221,7 @@ export default class NewTourDate extends React.Component {
                 </button>
               </div>
               <div className="modal-body">
-                <form action="" className="row d-flex justify-content-center mb-5" id="new-tour-date-form" onSubmit={this.handleSubmit}>
+                <form action="" className="row d-flex  mb-5" id="new-tour-date-form" onSubmit={this.handleSubmit}>
                   <div className="col-12 mb-2 mt-2">
                     <label htmlFor="artist-select" className="font-weight-bold">Artist</label>
                     <select id="artist-select" className="form-control fw-light" onChange={this.handleNameChange}>
@@ -240,6 +240,10 @@ export default class NewTourDate extends React.Component {
                     <input name="date" type="date" className="form-control fw-light" onChange={this.handleDateChange} />
                   </div>
                   <div className="col-12 mb-2 mt-2">
+                    <label htmlFor="venue" className="font-weight-bold">Venue</label>
+                    <input name="venue" type="text" className="form-control" onChange={this.handleVenueNameChange} />
+                  </div>
+                  <div className="col-12 mb-2 mt-2">
                     <label htmlFor="address" className="font-weight-bold">Location</label>
                     <input name="address" type="text" placeholder="Address" className="form-control" onChange={this.handleAddressChange} />
                     <div className="d-flex">
@@ -247,29 +251,42 @@ export default class NewTourDate extends React.Component {
                       <input name="state" type="text" placeholder='State' className="form-control mt-2 mb-2 ml-2 mr-2" onChange={this.handleStateChange} />
                       <input name="country" type="text" placeholder='USA' className="form-control mt-2 mb-2 ml-2" onChange={this.handleCountryChange} />
                     </div>
-
+                  </div>
+                  <div className="col-12 mb-2 mt-2">
+                    <label htmlFor="notes" className="font-weight-bold">Notes</label>
+                    <textarea name="notes" id="" className="form-control" onChange={this.handleNotesChange} />
+                  </div>
+                  <div className="col-12 mb-2 mt-2">
+                    <label htmlFor="contacts" className="font-weight-bold">Contact</label>
+                    <input type="text" className="form-control mt-2 mb-2" placeholder='Name' onChange={this.handleContactNameChange} />
+                    <input type="text" className='form-control mt-2 mb-2' placeholder='Phone' onChange={this.handleContactPhoneChange} />
+                    <input type="text" className='form-control mt-2 mb-2' placeholder='Email' onChange={this.handleContactEmailChange} />
                   </div>
                   <label htmlFor="" className="ml-2 font-weight-bold col-12">Schedule</label>
-                  <ul>
+                  <ul className="col-12">
                     {
                       this.state.scheduleEvents.map((event, index) => {
                         return (
-                          <li className={event.class} id={event.id} key={event.id}>
-                            <div className="col-6">
-                              <label htmlFor="start-time" className="text-center col-12">Start Time</label>
-                              <input type="time" className="form-control h-50" onChange={event => this.handleStartTimeChange(event, index)} />
+                          <li id={event.id} key={event.id}>
+                            <div className="d-flex">
+                              <div className="col-6">
+                                <label htmlFor="start-time" className="text-center">Start Time</label>
+                                <input type="time" className="form-control" onChange={event => this.handleStartTimeChange(event, index)} />
+                              </div>
+                              <div className="col-6">
+                                <label htmlFor="end-time" className="text-center">End Time</label>
+                                <input type="time" className="form-control" onChange={event => this.handleEndTimeChange(event, index)} />
+                              </div>
                             </div>
-                            <div className="col-6">
-                              <label htmlFor="end-time" className="text-center col-12">End Time</label>
-                              <input type="time" className="form-control h-50" onChange={event => this.handleEndTimeChange(event, index)} />
-                            </div>
-                            <button type="button" className="remove-schedule-event-btn col-1 d-flex align-items-center mt-3 bg-transparent border-0" onClick={this.handleClick}>
-                              <i className="fa-solid fa-x text-white" id={event.id} />
-                            </button>
-                            <div className="col-12 mt-3">
-                              <div className="row d-flex justify-content-center">
-                                <label htmlFor="details" className="text-center col-12">Details</label>
-                                <input type="text" className="form-control col-8 h-50" onChange={event => this.handleScheduleDetailsChange(event, index)} />
+                            <div className="d-flex">
+                              <div className="col-11">
+                                <label htmlFor="details" className="text-center">Details</label>
+                                <input type="text" className="form-control" onChange={event => this.handleScheduleDetailsChange(event, index)} />
+                              </div>
+                              <div className="col-1">
+                                <button type="button" className="remove-schedule-event-btn bg-transparent border-0 mt-2 pr-5" onClick={this.handleClick}>
+                                  <i className="fa-solid fa-x text-dark fa-lg mt-5" id={event.id} />
+                                </button>
                               </div>
                             </div>
                           </li>
@@ -280,11 +297,12 @@ export default class NewTourDate extends React.Component {
                   <button type="button" className="add-schedule-event-btn col-12 d-flex justify-content-center mt-3 bg-transparent border-0">
                     <i className="fa-solid fa-plus" onClick={this.addScheduleEvent} />
                   </button>
+                  {/* 🤔👇🏼 */}
+                  <div className="d-flex justify-content-end">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Save changes</button>
+                  </div>
                 </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
               </div>
             </div>
           </div>
@@ -358,27 +376,27 @@ export default class NewTourDate extends React.Component {
             </div>
             <div className="col-12 col-lg-6 form-group mt-3">
               <div className="row d-flex justify-content-center">
-                <label htmlFor="venue" className="col-12 text-center">VENUE</label>
+                {/* <label htmlFor="venue" className="col-12 text-center">VENUE</label>
                 <input
                   name="venue"
                   type="text"
                   className="form-control col-6"
-                  onChange={this.handleVenueNameChange} />
+                  onChange={this.handleVenueNameChange} /> */}
 
               </div>
               <div className="row d-flex justify-content-center mt-5">
-                <label htmlFor="notes" className="col-12 text-center">NOTES</label>
-                <textarea name="notes" id="" className="form-control col-8" onChange={this.handleNotesChange} />
+                {/* <label htmlFor="notes" className="col-12 text-center">NOTES</label>
+                <textarea name="notes" id="" className="form-control col-8" onChange={this.handleNotesChange} /> */}
               </div>
               <div className="row d-flex justify-content-center mt-5">
-                <label
+                {/* <label
                   htmlFor="contacts"
                   className="col-12 text-center"
                 >CONTACT</label>
 
                 <input type="text" className="form-control col-8 m-1" placeholder='Name' onChange={this.handleContactNameChange} />
                 <input type="text" className='form-control col-8 m-1' placeholder='Phone' onChange={this.handleContactPhoneChange} />
-                <input type="text" className='form-control col-8 m-1' placeholder='Email' onChange={this.handleContactEmailChange} />
+                <input type="text" className='form-control col-8 m-1' placeholder='Email' onChange={this.handleContactEmailChange} /> */}
 
               </div>
             </div>
