bab275d89f1552c199f396a77281758941d85ce7
Nicolas Sturz <nicksturz92@gmail.com>
2023-05-26 20:53:56 +0000

    finished ui-redesign-2.0-instructions

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -5,16 +5,14 @@ export default class NavBar extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg">
-          <a className="navbar-brand ml-5" href="#">TOUR MANAGER PRO</a>
-          <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="#">Tour Manager Pro</a>
+          <button className="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a href="#instructions">INSTRUCTIONS</a>
-              </li>
-            </ul>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#instructions">Instructions</a>
+            </div>
           </div>
         </nav>
       </div>
